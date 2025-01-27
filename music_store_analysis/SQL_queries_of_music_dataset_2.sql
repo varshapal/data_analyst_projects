1507b7f7f22bb9acb6e5fbd2548f9efd0218deed
varshapal <35573741+varshapal@users.noreply.github.com>
2025-01-27 21:23:28 +0530

    more SQL queries

---
+++ music_store_analysis/SQL_queries_of_music_dataset_2.sql
@@ -0,0 +1,59 @@
+---Medium Level Questions
+
+/*1- Write query to return the email, first name, last name and genre of all rock music 
+listeners. Return your list ordered alphabetically by email starting with A. */
+
+SELECT DISTINCT email, first_name, last_name
+FROM customer 
+JOIN invoice
+ON customer.customer_id = invoice.customer_id
+JOIN invoice_line
+ON invoice.invoice_id = invoice_line.invoice_id
+JOIN track
+ON invoice_line.track_id = track.track_id
+JOIN genre
+ON track.genre_id = track.genre_id
+WHERE genre.name = 'Rock'
+ORDER BY customer.email;
+
+
+/* 2- Let's invite the artist who have written the most rock music in our dataset. 
+write a query that returns the Artist name and total track count of the top 10 rock 
+bands.*/
+
+SELECT artist.artist_id, artist.name, COUNT(artist.artist_id) AS number_of_songs
+FROM artist
+JOIN album ON artist.artist_id = album.artist_id
+JOIN track ON album.album_id = track.album_id
+JOIN genre ON track.genre_id = genre.genre_id
+WHERE genre.name = 'Rock'
+GROUP BY artist.artist_id
+ORDER BY number_of_songs DESC
+LIMIT 10;
+
+
+/* Return all the track names that have a song length longer than the average 
+song length. Return the name and milliseconds for each track order by the song 
+length with the longest songs listed first. */
+
+SELECT name, milliseconds FROM track
+WHERE milliseconds > (
+	SELECT AVG(milliseconds) AS avg_song_length FROM track
+	)
+ORDER BY milliseconds DESC;
+
+
+
+
+------ADVANCE LEVEL QUESTION------------
+
+/* Find how much amount spend by each customer on artists? Write a query to return 
+customer name, artist name nd total spent. */
+
+SELECT customer.first_name, customer.last_name, invoice.total
+FROM customer
+JOIN invoice ON customer.customer_id = invoice.customer_id
+WHERE 
+
+
+	
